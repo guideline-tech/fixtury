008c57c765ac605480064530963380f6e5a8e28f
Aaron Averbuch <10715707+ahaverbuch@users.noreply.github.com>
2025-04-08 11:06:24 -0400

    Version 2.2.0 release (#112)
    * Version 2.2.0 release
    
    * Version 2.2.0 release

--- lib/fixtury/version.rb
+++ lib/fixtury/version.rb
@@ -3,8 +3,8 @@
 module Fixtury
 
   MAJOR       = 2
-  MINOR       = 1
-  PATCH       = 1
+  MINOR       = 2
+  PATCH       = 0
   PRERELEASE  = nil
 
   VERSION = [MAJOR, MINOR, PATCH, PRERELEASE].compact.join(".")
